54e76cca72e64c5d66090921f7866fabe3823ff3
glckfndr <obulan65@gmail.com>
2024-03-03 23:20:14 +0200

    test: Add Greet

---
+++ tests/components/Greet.test.tsx
@@ -0,0 +1,31 @@
+//import { it, expect, describe } from "vitest";
+import { render, screen } from '@testing-library/react'
+import Greet from "../../src/components/Greet";
+
+describe("Greet", () => {
+  it("should render Hello name when the name is provided", () => {
+    render(<Greet name="John" />);
+    //screen.debug();
+    const heading = screen.getByRole('heading');
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent(/John/i)
+  });
+
+  it("should render login button when the name is not provided", () => {
+    render(<Greet/>);
+    //screen.debug();
+    const heading = screen.queryByRole('heading');
+    expect(heading).not.toBeInTheDocument();
+
+    const button = screen.getByRole('button');
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent(/login/i)
+  });
+
+  it("should not render name when the name is not provided", () => {
+    render(<Greet/>);
+    //screen.debug();
+    const heading = screen.queryByRole('heading');
+    expect(heading).not.toBeInTheDocument();
+  });
+});
