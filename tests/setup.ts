8642f2c1ac6cf72c4b6a52b53fb18a2986b06792
glckfndr <obulan65@gmail.com>
2024-04-03 23:16:36 +0300

    config: add mock server

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,5 +1,10 @@
-import '@testing-library/jest-dom/vitest'
-import ResizeObserver from 'resize-observer-polyfill'
+import "@testing-library/jest-dom/vitest";
+import ResizeObserver from "resize-observer-polyfill";
+import { server } from "./mocks/server";
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 global.ResizeObserver = ResizeObserver;
 
@@ -7,10 +12,9 @@ window.HTMLElement.prototype.scrollIntoView = vi.fn();
 window.HTMLElement.prototype.hasPointerCapture = vi.fn();
 window.HTMLElement.prototype.releasePointerCapture = vi.fn();
 
-
-Object.defineProperty(window, 'matchMedia', {
+Object.defineProperty(window, "matchMedia", {
   writable: true,
-  value: vi.fn().mockImplementation(query => ({
+  value: vi.fn().mockImplementation((query) => ({
     matches: false,
     media: query,
     onchange: null,
@@ -20,4 +24,4 @@ Object.defineProperty(window, 'matchMedia', {
     removeEventListener: vi.fn(),
     dispatchEvent: vi.fn(),
   })),
-})
+});
